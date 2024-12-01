94201785a939f636134807c3d3f747245e6fd212
Dmytry64 <nosdm64@gmail.com>
2024-12-01 12:05:57 +0300

    feat: добавляет react & лоадеры для стилей

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-
-document.body.innerHTML = `<div>Привет Юпсс Ctdth</div>`
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import './index.scss'
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <div className='root'>Привет Димаwewewew</div>
+);
